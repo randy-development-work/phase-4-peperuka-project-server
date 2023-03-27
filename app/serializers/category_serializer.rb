b9f42b60c03e1a5a8a7e6f2b2ca7c12c5fd5ccf0
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-27 22:18:35 +0300

    1. Add Procfile to simultaneously run backend and frontend. 2. Scaffold add Category. 3. Add validation for name attribute of the Category model. 4. Add Category serializer. 5. Seed Category data.

---
+++ app/serializers/category_serializer.rb
@@ -0,0 +1,3 @@
+class CategorySerializer < ActiveModel::Serializer
+  attributes :name, :image
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.create([
+    {
+        name: "Food",
+        image: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+        name: "Gas",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWdrlopCOIYT6rrFeEE8uRdgMj-EvhphPK_jVbrfekweFK5pKI5Um5Zjq2im9xUgAMavQ&usqp=CAU"
+    },
+    {
+        name: "Groceries",
+        image: "https://images.pexels.com/photos/128402/pexels-photo-128402.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+        name: "Beverages",
+        image: "https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+        name: "Supermarket",
+        image: "https://images.pexels.com/photos/3017260/pexels-photo-3017260.jpeg?auto=compress&cs=tinysrgb&w=400"
+        # https://images.pexels.com/photos/5380919/pexels-photo-5380919.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load
+    },
+    {
+        name: "Medicine",
+        image: "https://images.pexels.com/photos/3652750/pexels-photo-3652750.jpeg?auto=compress&cs=tinysrgb&w=400"
+    }
+])
