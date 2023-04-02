5b1307c738df9eb32e6a23a618f33302115c9cfd
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-04-02 09:12:54 +0300

    Reset deployment files.

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,5 +41,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-port ENV['RAILS_ENV'] || 4567
